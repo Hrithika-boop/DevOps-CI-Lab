05523e122af0d0c8d9c183b6ec8a376de718f512
Hrithika-boop <malkarihrithika@gmail.com>
2025-07-14 21:17:02 +0530

    Modified App.java to trigger Jenkins build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Hello Jenkins! This is auto build test.");
 
     }
 
